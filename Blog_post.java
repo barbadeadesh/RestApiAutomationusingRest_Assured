4af8e86b67dd2c49f0f23c7cc5443881825ba355
barbadeadesh <barbadeadesh2020@email.com>
2022-08-23 09:08:13 +0530

    ADDED: Functionality added for blog posts

---
+++ Blog_post.java
@@ -0,0 +1,66 @@
+package com.bridgelabz.RestApi_CQA107;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "id", "name", "field" })
+
+public class BlogPosts1 {
+	@JsonProperty("id")
+	private Integer id;
+	@JsonProperty("name")
+	private String name;
+	@JsonProperty("field")
+	private String field;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	@JsonProperty("id")
+	public Integer getId() {
+		return id;
+	}
+
+	@JsonProperty("id")
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@JsonProperty("name")
+	public String getName() {
+		return name;
+	}
+
+	@JsonProperty("name")
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@JsonProperty("field")
+	public String getField() {
+		return field;
+	}
+
+	@JsonProperty("field")
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+}
